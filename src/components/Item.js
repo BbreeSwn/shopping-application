268bbcf5ff82dec5c3b7013640e5325a3a503942
BbreeSwn <suwanan.cs@hotmail.com>
2024-09-16 16:18:25 +0700

    feat : cart style

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,18 @@
+import "./Item.css"
+
 export default function Item(props) {
   const { id, name, price, image, quantity } = props;
   return (
     <div className="card">
       <img src={image} alt={id} />
       <div className="product">
-        <p className="name">Name : {props.name}</p>
-        <p className="price">Price : {props.price}</p>
+        <p className="name">Name : {name}</p>
+        <p className="price">Price : {price}</p>
       </div>
       <div className="quantity">
-        <button>-</button>
+        <button className="decrease">-</button>
         <input type="text" value={quantity} disabled />
-        <button>+</button>
+        <button className="add">+</button>
       </div>
       <div className="total-price">{quantity * price}</div>
       <button>Delete item</button>
