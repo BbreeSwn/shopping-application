dd6faf5eef4d6be1bca26c5ea0ecd7d704bec354
BbreeSwn <suwanan.cs@hotmail.com>
2024-09-16 12:49:31 +0700

    feat:set shopping app

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css"
+
+export default function Header() {
+  return (
+    <div>
+        <header>
+            <p>Shopping Application</p>
+            <p>Shopping cart : 0</p>
+        </header>
+    </div>
+  )
+}
